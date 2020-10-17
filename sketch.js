d48c546c8de52bac494a21f02da6c56e550ac6fe
AdhirajDatar <68225977+AdhirajDatar@users.noreply.github.com>
2020-10-17 12:25:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,127 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var p ;
+var particles = [];
+var plinkos = [];
+var divisions = [];
+
+var turn = 0
+
+var divisionHeight=300;
+var score =0;
+
+var gameState = "start";
+
+
+var line
+
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+     
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+
+  text("500",20,520);
+  text("500",100,520);
+  text("500",180,520);
+  text("500",260,520);
+  text("1000",340,520);
+  text("1000",420,520);
+  text("1000",500,520);
+  text("200",580,520);
+  text("200",660,520);
+  text("200",740,520);
+
+  text("Score : "+score,20,30);
+  Engine.update(engine);
+
+   for (var i = 0; i < plinkos.length; i++) {
+     plinkos[i].display();
+   }
+ 
+   for (var k = 0; k < divisions.length; k++) {
+     divisions[k].display();
+   }
+    if (p != null)
+    { 
+      p.display();
+
+      if(p.body.position.y>760)
+      {
+        if (p.body.position.x<300)
+        {
+          score=score+500;
+          p=null;
+          if(turn=5)gameState = "end"
+        }
+
+       else if (p.body.position.x>301 && p.body.position.x<580 )
+        {
+          score=score+1000;
+          p=null;
+          if(turn=5)gameState = "end"
+        }
+
+      else  if (p.body.position.x>581 && p.body.position.x<900 )
+        {
+          score=score+200;
+          p=null;
+          if(turn=5)gameState = "end"
+        }
+      }
+    }
+
+    if(gameState === "end"){
+      
+      textSize(40);
+      stroke("green");
+      strokeWeight(10)
+      text("GAME KHATAM BHAISHAB", 100,330);
+    }
+}
+
+function mousePressed(){
+  if (gameState === "start"){
+    turn +=1
+    p = new Particle(mouseX,10,5);
+  }
+}
